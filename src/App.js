7e9cff487d7c933f8231e6b83726f5afdaddf5c0
P1rc <leftakk4smth@gmail.com>
2024-10-06 22:39:05 +0300

    fix style 3

--- src/App.js
+++ src/App.js
@@ -11,12 +11,14 @@ export const App = () => {
     const [count, setCount] = useState(0);
 
     const increment = () => setCount(count + 1);
+    const reset = () => setCount(0);
 
     return (
         <div style={{ textAlign: 'center', marginTop: '50px' }}>
-            <h1 style={{ color: 'cyan' }}>Counter: {count}</h1>
+            <h1 style={{ color: 'orange' }}>Counter: {count}</h1>
             <div>
                 <button onClick={increment} style={buttonStyle}>+1</button>
+                <button onClick={reset} style={buttonStyle}>Reset</button>
             </div>
         </div>
     );
